5ae41db23e5d7e5b2e15b098b21ebfa3c979f97c
ntdang <ndang83@gmail.com>
2018-03-06 12:46:53 -0500

    Put into columns

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,4 +1,27 @@
 $(document).ready(function () {
+  $("#addShow").on("click", function () {
+    event.preventDefault();
+
+    if ($('#showInput').val() === '') {
+      $('#showInput').addClass('is-invalid');
+      return false;
+    }
+    $('#showInput').removeClass('is-invalid');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
 
 
 
